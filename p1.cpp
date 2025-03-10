f7a445ac5b7cf1730b13a4a31dcc0047fed5efdb
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-03-10 11:53:26 +0700

    prosedur

---
+++ p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+float luas, panjang, lebar; //var global
+
+void inputdata(){
+    cout << "masukkan panjangnya";
+    cin >> panjang;
+
+    cout << "masukkan lebarnya";
+    cin >> lebar;
+
+void prosedurluas();
+        luas = panjang * lebar;
+    }
+
+ void tampilkanluas(){
+        cout << "luas persegi panjang:" << luas << endl;
+    
+}
